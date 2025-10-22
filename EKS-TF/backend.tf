535a9c72f2eecd89551253a68200eb735ad3f897
anveshsalendra <anveshsalendra1999@gmail.com>
2025-10-22 08:52:03 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "robo2.0" # Replace with your actual S3 bucket name
+    bucket = "robo2.1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
