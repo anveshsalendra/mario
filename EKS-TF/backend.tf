0171cb90998306915072e053d6b32443a08621a9
anveshsalendra <anveshsalendra1999@gmail.com>
2025-10-30 07:43:27 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "supermario2" # Replace with your actual S3 bucket name
+    bucket = "supermario2.1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
